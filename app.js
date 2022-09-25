b411b8ff81376e61d840434c8190f3ed767e8162
pcbennett108 <pcbennett42@gmail.com>
2022-09-25 11:50:09 -0600

    Implement Mongoose

--- app.js
+++ app.js
@@ -1,50 +1,21 @@
-const MongoClient = require("mongodb").MongoClient;
-const assert = require("assert");
+const mongoose = require("mongoose");
 
-const url = "mongodb://localhost:27017";
-
-const dbName = "newFruitsDB";
-
-const client = new MongoClient(url, { useNewUrlParser: true });
-
-client.connect(function (err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  insertDocuments(db, function () {
-    client.close();
-  });
+mongoose.connect("mongodb://localhost:27017/fruitsDB", {
+  useNewUrlParser: true,
 });
 
-const insertDocuments = function (db, callback) {
-  const collection = db.collection("fruits");
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String,
+});
 
-  collection.insertMany(
-    [
-      {
-        name: "Apple",
-        rating: 8,
-        review: "Great fruit",
-      },
-      {
-        name: "Orange",
-        rating: 6,
-        review: "Kinda sour",
-      },
-      {
-        name: "Banana",
-        rating: 9,
-        review: "Great stuff!",
-      },
-    ],
-    function (err, result) {
-      assert.equal(err, null);
-      /* assert.equal(3, result.result.n); */
-      /* assert.equal(3, result.ops.length); */
-      console.log("Inserted 3 documents into the collection");
-      callback(result);
-    }
-  );
-};
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const fruit = new Fruit({
+  name: "Apple",
+  rating: 7,
+  review: "Pretty solid as a fruit.",
+});
+
+fruit.save();
